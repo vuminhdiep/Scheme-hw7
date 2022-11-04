f7a71de6327360fe132c3104d5eaf61694858333
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2022-11-04 19:28:05 -0400

    finish copy vector

--- store.scm
+++ store.scm
@@ -1,30 +1,61 @@
 ;; store.scm
+
+;;complexity of vector-ref?
+
 (define the-store! 'uninitialized)
+(define store-size! 'uninitialized)   ;;Ask MATT what is the best design practice for the store datastructure
+(define STORE-MAXSIZE 1000)
+
+(define copy-vector (lambda [small-v big-v idx]
+    (if (= idx (vector-length small-v)) 
+      (void)
+      [begin
+      (vector-set! big-v idx (vector-ref small-v idx))
+      (copy-vector small-v big-v (+ 1 idx))
+      ]
+    )
+  )
+)
+; (define double-store! (lambda () [
+;     (let [[new-store (make-vector (* 2 (vector-length the-store!)))]]   
+
+;     )
+;   ]
+; ))
 
 ;; (empty-store) returns an empty Scheme list representing the empty store.
-(define empty-store (lambda () '()))
+;(define empty-store (lambda () '()))
 
 ;vector empty-store
-(define empty-store (make-vector 0))
+
+(define empty-store (make-vector STORE-MAXSIZE))
 
 ;; (initialize-store!) initializes the-store! to (empty-store).
 (define initialize-store!
-  (lambda () (set! the-store! (empty-store)) ))
+  (lambda () (set! the-store! (empty-store)) (set! store-size! 0) ))
 
 ;; (newref! ev) takes an expval ev adds to the-store! and returns
 ;; a ref-val that points to the added value.
-(define newref!
-  (lambda (ev)
-    (set! the-store! (append the-store! (list ev)))
-    (ref-val (- (length the-store!) 1))
-    ))
+; (define newref!
+;   (lambda (ev)
+;     (set! the-store! (append the-store! (list ev)))
+;     (ref-val (- (length the-store!) 1))
+;     ))
+
+; (define newref! 
+;   (lambda (val)  ;;val = Scheme's value
+;     (if (>= store-size! (vector-length the-store!))
+
+;     )
+;   )
+; )
 
 ;; (deref ev) takes an expressed value which should be a (ref-val ref)
 ;; and returns the value associated with ref in the-store!.
-(define deref
-  (lambda (ev)
-    (list-ref the-store! (expval->ref ev))
-    ))
+; (define deref
+;   (lambda (ev)
+;     (list-ref the-store! (expval->ref ev))
+;     ))
 
 ;vector deref
 (define deref
@@ -34,18 +65,18 @@
 
 ;; (setref! ev1 ev2) takes two expvals, the first which should be a
 ;; (ref-val ref) it sets the cell ref in the-store! to ev2.  Returns (unit-val).
-(define setref!
-  (lambda (ev1 ev2)
-    (let [[ref (expval->ref ev1)]]
-      (set! the-store! (setref!* the-store! ref ev2))
-      (unit-val)
-    )))
+; (define setref!
+;   (lambda (ev1 ev2)
+;     (let [[ref (expval->ref ev1)]]
+;       (set! the-store! (setref!* the-store! ref ev2))
+;       (unit-val)
+;     )))
 
-(define setref!*
-  (lambda (store ref ev)
-    (cond
-      [(= 0 ref) (cons ev (cdr store))]
-      [else (cons (car store) (setref!* (cdr store) (- ref 1) ev))])))
+; (define setref!*
+;   (lambda (store ref ev)
+;     (cond
+;       [(= 0 ref) (cons ev (cdr store))]
+;       [else (cons (car store) (setref!* (cdr store) (- ref 1) ev))])))
 
 ;;vector setref!
 (define setref!
